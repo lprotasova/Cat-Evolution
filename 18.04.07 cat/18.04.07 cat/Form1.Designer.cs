7b5c29ad46cbd77a728db3b344a5b68e02b923d1
lprotasova <lprotasova@carthage.edu>
2018-04-07 20:20:35 -0500

    Initial commit
    it works

---
+++ 18.04.07 cat/18.04.07 cat/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace _18._04._07_cat
+{
+	partial class mainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.scoreLabel = new System.Windows.Forms.Label();
+			this.dog1 = new System.Windows.Forms.PictureBox();
+			this.food1 = new System.Windows.Forms.PictureBox();
+			this.cat1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.dog1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.food1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.cat1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(543, 372);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(0, 16);
+			this.label1.TabIndex = 1;
+			// 
+			// scoreLabel
+			// 
+			this.scoreLabel.AutoSize = true;
+			this.scoreLabel.Location = new System.Drawing.Point(493, 372);
+			this.scoreLabel.Name = "scoreLabel";
+			this.scoreLabel.Size = new System.Drawing.Size(57, 16);
+			this.scoreLabel.TabIndex = 2;
+			this.scoreLabel.Text = "Score: 0";
+			// 
+			// dog1
+			// 
+			this.dog1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.dog1.Location = new System.Drawing.Point(130, 245);
+			this.dog1.Name = "dog1";
+			this.dog1.Size = new System.Drawing.Size(50, 50);
+			this.dog1.TabIndex = 4;
+			this.dog1.TabStop = false;
+			// 
+			// food1
+			// 
+			this.food1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.food1.Location = new System.Drawing.Point(375, 150);
+			this.food1.Name = "food1";
+			this.food1.Size = new System.Drawing.Size(50, 50);
+			this.food1.TabIndex = 3;
+			this.food1.TabStop = false;
+			// 
+			// cat1
+			// 
+			this.cat1.BackgroundImage = global::_18._04._07_cat.Properties.Resources.c1;
+			this.cat1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.cat1.Location = new System.Drawing.Point(0, 0);
+			this.cat1.Name = "cat1";
+			this.cat1.Size = new System.Drawing.Size(50, 50);
+			this.cat1.TabIndex = 0;
+			this.cat1.TabStop = false;
+			// 
+			// mainForm
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(255)))), ((int)(((byte)(145)))));
+			this.ClientSize = new System.Drawing.Size(600, 397);
+			this.Controls.Add(this.dog1);
+			this.Controls.Add(this.food1);
+			this.Controls.Add(this.scoreLabel);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.cat1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "mainForm";
+			this.Text = "Cat Factory";
+			((System.ComponentModel.ISupportInitialize)(this.dog1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.food1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.cat1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox cat1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label scoreLabel;
+		private System.Windows.Forms.PictureBox food1;
+		private System.Windows.Forms.PictureBox dog1;
+	}
+}
+
---
+++ 18.04.07 cat/18.04.07 cat/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _18._04._07_cat.Properties;
+using System.Diagnostics;
+using System.Media;
+
+namespace _18._04._07_cat
+{
+	public partial class mainForm : Form
+	{
+		public Stopwatch timer = new Stopwatch();
+		public SoundPlayer player = new SoundPlayer();
+
+		public mainForm()
+		{
+			InitializeComponent();
+			KeyDown += new KeyEventHandler(mainForm_KeyDown);
+			spawnFood(600, 400);
+			spawnDog(600, 400);
+			timer.Start();
+			//player.SoundLocation = "Resources/nyan1.mp3";
+			//player.PlayLooping();
+		}
+
+		/*private void panel1_DragEnter(object sender, ItemDragEventArgs e)
+		{
+			MessageBox.Show("dragenter");
+
+			if (e.Data.GetDataPresent(typeof(Bitmap)))
+				e.Effect = DragDropEffects.Copy;
+			else
+				e.Effect = DragDropEffects.None;
+		}*/
+
+		private void mainForm_KeyDown(object sender, KeyEventArgs e)
+		{
+			int x = cat1.Location.X;
+			int y = cat1.Location.Y;
+			int stepVal = 50;
+			int formSizeX = 600;
+			int formSizeY = 400;
+
+			//handle for keys we use
+			if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+				x += stepVal;
+			else if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+				x -= stepVal;
+			else if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+				y -= stepVal;
+			else if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+				y += stepVal;
+			else if (e.KeyCode == Keys.Escape)
+				Application.Exit();
+
+			//ensure we don't go off-screen
+			if (x >= 0 && x + 50 <= formSizeX && y >= 0 && y + 50 <= formSizeY)
+				cat1.Location = new System.Drawing.Point(x, y);
+
+			//handle collision with food
+			if (x == food1.Location.X && y == food1.Location.Y)
+			{
+				spawnFood(formSizeX, formSizeY);
+				spawnDog(formSizeX, formSizeY);
+				int score = Convert.ToInt32(scoreLabel.Text.Substring(7));
+				score++;
+				//score += 100;
+				scoreLabel.Text = "Score: " + score;
+
+				switch ((int) Math.Log(score, 2))
+				{
+					case 2:
+						cat1.BackgroundImage = Resources.c2;
+						break;
+					case 3:
+						cat1.BackgroundImage = Resources.c3;
+						break;
+					case 4:
+						cat1.BackgroundImage = Resources.c4;
+						break;
+					case 5:
+						cat1.BackgroundImage = Resources.c5;
+						break;
+					case 6:
+						cat1.BackgroundImage = Resources.c6;
+						break;
+					case 7:
+						cat1.BackgroundImage = Resources.c7;
+						break;
+					case 8:
+						cat1.BackgroundImage = Resources.c8;
+						break;
+					case 9:
+						cat1.BackgroundImage = Resources.c9;
+						break;
+					case 10:
+						cat1.BackgroundImage = Resources.c10;
+						timer.Stop();
+						player.Stop();
+						MessageBox.Show("YOU HAVE EVOLVED TO ULTIMATE CAT. YOUR TIME IS " + timer.ElapsedMilliseconds / 60000 +
+							" MINUTES, " + (timer.ElapsedMilliseconds % 60000) / 1000 + " SECONDS. PLEASE RECORD YOUR TIME ON THE LIST OF CHAMPIONS.");
+						Application.Exit();
+						break;
+					default:
+						break;
+				}
+			}
+			//handle collision with dog
+			else if (x == dog1.Location.X && y == dog1.Location.Y)
+			{
+				MessageBox.Show("BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK " +
+					"BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK " +
+					"BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK BARK.\n\n" + 
+					"(your score was " + scoreLabel.Text.Substring(7) + ", please record this on the high score list)", "BARK.", MessageBoxButtons.OK);
+				Application.Exit();
+			}
+		}
+
+		private void spawnFood(int screenX, int screenY)
+		{
+			Random rand = new Random();
+			int foodX = rand.Next(0, screenX / 50);
+			int foodY = rand.Next(0, screenY / 50);
+
+			//while loop of re-loading to prevent spawn onto cat
+			while (foodX * 50 == cat1.Location.X && foodY * 50 == cat1.Location.Y)
+			{
+				foodX = rand.Next(0, screenX / 50);
+				foodY = rand.Next(0, screenY / 50);
+			}
+
+			food1.Location = new System.Drawing.Point(foodX * 50, foodY * 50);
+			food1.BackgroundImage = Resources.f1;
+		}
+
+		private void spawnDog(int screenX, int screenY)
+		{
+			Random rand = new Random();
+			int dogX = rand.Next(0, screenX / 50);
+			int dogY = rand.Next(0, screenY / 50);
+
+			//while loop of re-loading to prevent spawn onto food or cat
+			while ((dogX * 50 == food1.Location.X && dogY * 50 == food1.Location.Y)
+				|| (dogX * 50 == cat1.Location.X && dogY * 50 == cat1.Location.Y))
+			{
+				dogX = rand.Next(0, screenX / 50);
+				dogY = rand.Next(0, screenY / 50);
+			}
+
+			dog1.Location = new System.Drawing.Point(dogX * 50, dogY * 50);
+			dog1.BackgroundImage = Resources.d1;
+		}
+	}
+}
